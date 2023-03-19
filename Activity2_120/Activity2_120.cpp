d3587c269a398aa1924c4c6a18827d8225452cad
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-03-20 11:56:40 +1200

    latihan3

--- Activity2_120/Activity2_120.cpp
+++ Activity2_120/Activity2_120.cpp
@@ -19,5 +19,58 @@ int main()
     float panjang, lebar, jejari, alas, tinggi;
     int pilihan;
     bool menu;
- 
+    menu = true;
+    while (menu == true)
+    {
+        cout << "\n\n===========";
+        cout << "\n M E N U";
+        cout << "\n=============";
+        cout << "\n 1. Luas Persegi Panjang";
+        cout << "\n 2. Luas Lingkaran";
+        cout << "\n 3. Luas Segitiga";
+        cout << "\n 4. Array";
+        cout << "\n 5. Keluar";
+        cout << "\n Pilihan (1/2/3/4/5) : ";
+        cin >> pilihan;
+
+
+        switch (pilihan)
+        {
+        case 1:
+            cout << "Masukan Panjang = ";
+            cin >> panjang;
+            cout << "Masukan Lebar = ";
+            cin >> lebar;
+            cout << "\n Luas Persegi Panjang = " << persegipanjang(panjang, lebar);
+            break;
+        case 2:
+            cout << "Masukan jari-jari =";
+            cin >> jejari;
+            cout << "\n Luas Lingkaran = " << lingkaran(jejari);
+            break;
+        case 3:
+            cout << "Masukan Alas = ";
+            cin >> alas;
+            cout << "Masukan Tinggi = ";
+            cin >> tinggi;
+            cout << "\n Luas Segitiga = " << segitiga(alas, tinggi);
+            break;
+        case 4:
+            cout << "Mengisi Array\n";
+            for(int i = 0; i < 10; i++) {
+                cout <<  "Nilai ke-" << (i + 1) << ": ";
+                cin >> nilai[i];
+            }
+            cout << "Membaca isi Array\n";
+            for (int i = 0; i < 10; i++) {
+                cout << "\nNilai ke-" << (i + 1) << ": " << nilai[i];
+            }
+            break;
+        case 5:
+            menu = false;
+        default:
+            cout << "Pilihan Salah !!";
+            break;
+        }
+    }
 }
