0543998e24cf695d0ed3ed3219aada912b4fbfbc
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-03-20 10:47:28 +1200

    Add project files.

---
+++ Activity2_120/Activity2_120.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+float persegipanjang(float p, float l) {
+    return p * l;
+}
+
+float lingkaran(float r) {
+    return 3.14 * r * r;
+  }
+
+float segitiga(float a, float t) {
+    return 0.5 * a * t;
+}
+
+int main()
+{
+    float nilai[10];
+    float panjang, lebar, jejari, alas, tinggi;
+    int pilihan;
+    bool menu;
+ 
+}
